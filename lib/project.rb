6a7c9030cd066f53746295216b09034e0d6bdd3d
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 21:04:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Project 
   attr_reader :title, :backers, 
   
@@ -8,6 +9,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer 
+    binding.pry
     # backer.back_project(self) 
   end
   
