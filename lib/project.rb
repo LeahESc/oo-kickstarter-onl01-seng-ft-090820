897d720db99897981a3f9c434cdadd096ba71bf1
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 21:14:30 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Project 
+  attr_reader :title, :backers, :backed_projects 
+  
+  def initialize(title)
+    @title = title 
+    @backers = [] 
+  end 
+  
+  def add_backer(backer)
+    # binding.pry
+    @backers << backer
+    backer.backed_projects << self
+  end
+  
+end 
